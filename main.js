4dabadf1cfb2cd2c01497b82d143d3c079bd70dc
iiTzJacob <jj.colon7802@gmail.com>
2023-01-17 22:53:55 -0500

    Finished evaluate function

--- main.js
+++ main.js
@@ -4,11 +4,15 @@ const numbers = document.querySelectorAll("#number");
 const clear = document.querySelector("#clear");
 const operators = document.querySelectorAll("#operator")
 const deleteButton = document.querySelector("#delete")
+const equalButton = document.querySelector("#equal")
 let inputString = "";
 let outputString = "";
+let initialNumber = "";
+let secondaryValue = "";
 
 clear.addEventListener("click", allClear);
 deleteButton.addEventListener("click", deleteItem)
+equalButton.addEventListener("click", evaluateFunction)
 
 numbers.forEach(nums => {
     nums.addEventListener("click", function () {
@@ -22,9 +26,11 @@ numbers.forEach(nums => {
                 return
             }
             inputString += nums.value
+            initialNumber = inputString
             inputNums.innerHTML = inputString
         } else {
             inputString += nums.value
+            initialNumber = inputString
             inputNums.innerHTML = inputString
         }
     })
@@ -37,7 +43,9 @@ operators.forEach(operator => {
             return
         }
         if (inputString != "") {
+            secondaryValue = initialNumber;
             inputString += operator.value;
+            outputString = inputString
             inputNums.innerHTML = null;
             output.innerHTML = inputString;
             inputString = "";
@@ -55,4 +63,29 @@ function allClear() {
 function deleteItem() {
     inputString = inputString.slice(0, -1);
     inputNums.innerHTML = inputString;
+}
+
+function evaluateFunction() {
+    outputString += inputString;
+    output.innerHTML = outputString;
+
+    if (outputString.includes("+")) {
+        let total = parseFloat(secondaryValue) + parseFloat(initialNumber)
+        inputNums.innerHTML = total.toString()
+    }
+
+    if (outputString.includes("-")) {
+        let total = parseFloat(secondaryValue) - parseFloat(initialNumber)
+        inputNums.innerHTML = total.toString()
+    }
+
+    if (outputString.includes("÷")) {
+        let total = parseFloat(secondaryValue) / parseFloat(initialNumber)
+        inputNums.innerHTML = total.toString()
+    }
+
+    if (outputString.includes("*")) {
+        let total = parseFloat(secondaryValue) * parseFloat(initialNumber)
+        inputNums.innerHTML = total.toString()
+    }
 }
